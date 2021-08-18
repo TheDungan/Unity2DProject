61a8c16dca7e0f0f771783d3d8ca3c7fa59db32d
TheDungan <89111046+TheDungan@users.noreply.github.com>
2021-08-17 18:15:35 -0600

    1st Save

---
+++ 2D Project 1/Assets/Scripts/DungeonGeneration/RoomChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomChange : MonoBehaviour
+{
+    public GameObject virtualCam;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            virtualCam.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            virtualCam.SetActive(false);
+        }
+    }
+}
